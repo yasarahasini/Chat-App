3a3471276aca04ee5acf70885e7aa5a5634640ea
yasara <yasarahasini61@gmail.com>
2025-08-29 20:59:58 +0530

    first

---
+++ app/user/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+export default function User() {
+  return (
+    <p>This is the user page</p>
+  );
+}
---
+++ app/user/settings/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+export default function Settings() {
+  return (
+<p>setting</p>
+  );
+}
